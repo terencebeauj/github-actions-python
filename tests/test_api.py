1e66234805104c1d64b4481b1d6b45cbc52a8479
Terence <beaujour.t@hotmail.fr>
2022-09-14 02:12:56 -0400

    Implement API tests

---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+"""Test for the API endpoints
+"""
+
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    """Test for the root endpoint
+    """
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "Ok"}
+
+def test_get_prediction():
+    """Test the prediction endpoint
+    """
+    response = client.get("/prediction/btcbusd")
+    assert response.status_code == 200
